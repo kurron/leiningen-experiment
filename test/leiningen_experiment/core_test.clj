673622cb8ce09fffb995e42c0959efed8ac765c4
Ron Kurr <kurr@kurron.org>
2013-09-16 15:31:50 -0400

    Compiling into .class files

---
+++ test/leiningen_experiment/core_test.clj
@@ -0,0 +1,7 @@
+(ns leiningen-experiment.core-test
+  (:require [clojure.test :refer :all]
+            [leiningen-experiment.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
