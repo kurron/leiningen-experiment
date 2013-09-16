9081d2cb1d2869bb26f1bf61c3582bb2d3d3da6f
Ron Kurr <kurr@kurron.org>
2013-09-16 16:13:19 -0400

    Added a Java interface for Clojure to implement.

--- project.clj
+++ project.clj
@@ -3,5 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :java-source-paths ["src/main/java"]
   :aot[leiningen-experiment.core]
   :dependencies [[org.clojure/clojure "1.5.1"]])
---
+++ src/main/java/org/kurron/Logan.java
@@ -0,0 +1,8 @@
+package org.kurron;
+
+/**
+ * A simple interface to exercise Java-to-Clojure integration.
+ */
+public interface Logan {
+    String echo( final String message );
+}
